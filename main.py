762b3f37736d68093c47990aab08784955966bc3
shotaro-ada <syouta2223505@gmail.com>
2023-04-22 06:57:00 +0000

    createEvent api

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, request
 import openai
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import json
 
-#firebaseのapiの設定
-cred = credentials.Certificate("./jinbee-cup-firebase-adminsdk-y7xm5-80b53cb07f.json")
+# firebaseのapiの設定
+cred = credentials.Certificate(
+    "./jinbee-cup-firebase-adminsdk-y7xm5-80b53cb07f.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 # chatgptのAPIキーの設定
-openai.api_key = os.environ["OPENAI_API_KEY"] 
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 
 # response = openai.ChatCompletion.create(
@@ -27,11 +29,26 @@ def send_to_database():
     # firestore.ArrayUnion　<=おまじない
     doc_ref.set({'key': 'value'})
 
-send_to_database()
 
-# app = Flask(__name__, static_folder='.', static_url_path='')
-# @app.route('/')
-# def index():
-#     return "this is test"
+app = Flask(__name__, static_folder='.', static_url_path='')
 
-# app.run(port=8000, debug=True)
+
+@app.route('/')
+def index():
+    return "this is test"
+
+
+@app.route('/createEvent', methods=["POST"])
+def create_event():
+    data = json.loads(request.get_data())['data']
+
+    doc_ref = db.collection("events").document(data["user_id"])
+    doc_ref.update({data["event_name"]: {
+        "date": data["date"],
+        "description": data["description"],
+        "tags": data["tags"]
+    }})
+    return data["user_id"]
+
+
+app.run(port=5002, debug=True)
